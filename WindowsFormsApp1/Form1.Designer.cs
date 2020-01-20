fd78b22351c92a58e3b4e195edff50bcae7c02bf
5.010 Nagib System <adrian2536086@yandex.ru>
2020-01-20 17:36:08 +0300

    Visual part 2

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
@@ -36,8 +37,15 @@
             this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
-            this.Timer1 = new System.Windows.Forms.Timer(this.components);
-            this.Timer2 = new System.Windows.Forms.Timer(this.components);
+            this.Timer0 = new System.Windows.Forms.Timer(this.components);
+            this.Timer1a = new System.Windows.Forms.Timer(this.components);
+            this.Timer1b = new System.Windows.Forms.Timer(this.components);
+            this.Timer2a = new System.Windows.Forms.Timer(this.components);
+            this.Timer2b = new System.Windows.Forms.Timer(this.components);
+            this.Timer3a = new System.Windows.Forms.Timer(this.components);
+            this.Timer3b = new System.Windows.Forms.Timer(this.components);
+            this.Timer4a = new System.Windows.Forms.Timer(this.components);
+            this.Timer4b = new System.Windows.Forms.Timer(this.components);
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -50,6 +58,7 @@
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.toolStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
@@ -70,7 +79,7 @@
             this.toolStripButton2});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(1262, 27);
+            this.toolStrip1.Size = new System.Drawing.Size(1000, 25);
             this.toolStrip1.TabIndex = 7;
             this.toolStrip1.Text = "toolStrip1";
             // 
@@ -80,7 +89,7 @@
             this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
             this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton1.Name = "toolStripButton1";
-            this.toolStripButton1.Size = new System.Drawing.Size(49, 24);
+            this.toolStripButton1.Size = new System.Drawing.Size(40, 22);
             this.toolStripButton1.Text = "Файл";
             // 
             // toolStripButton4
@@ -90,28 +99,28 @@
             this.toolStripMenuItem3,
             this.toolStripMenuItem4});
             this.toolStripButton4.Name = "toolStripButton4";
-            this.toolStripButton4.Size = new System.Drawing.Size(98, 24);
+            this.toolStripButton4.Size = new System.Drawing.Size(80, 22);
             this.toolStripButton4.Text = "Настройки";
             // 
             // toolStripMenuItem2
             // 
             this.toolStripMenuItem2.Name = "toolStripMenuItem2";
             this.toolStripMenuItem2.ShowShortcutKeys = false;
-            this.toolStripMenuItem2.Size = new System.Drawing.Size(169, 26);
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(135, 22);
             this.toolStripMenuItem2.Text = "Настройка 1";
             // 
             // toolStripMenuItem3
             // 
             this.toolStripMenuItem3.Name = "toolStripMenuItem3";
             this.toolStripMenuItem3.ShowShortcutKeys = false;
-            this.toolStripMenuItem3.Size = new System.Drawing.Size(169, 26);
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(135, 22);
             this.toolStripMenuItem3.Text = "Настройка 2";
             // 
             // toolStripMenuItem4
             // 
             this.toolStripMenuItem4.Name = "toolStripMenuItem4";
             this.toolStripMenuItem4.ShowShortcutKeys = false;
-            this.toolStripMenuItem4.Size = new System.Drawing.Size(169, 26);
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(135, 22);
             this.toolStripMenuItem4.Text = "Настройка 3";
             // 
             // toolStripButton2
@@ -120,8 +129,44 @@
             this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
             this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton2.Name = "toolStripButton2";
-            this.toolStripButton2.Size = new System.Drawing.Size(73, 24);
+            this.toolStripButton2.Size = new System.Drawing.Size(60, 22);
             this.toolStripButton2.Text = "Помощь";
+            // 
+            // Timer0
+            // 
+            this.Timer0.Tick += new System.EventHandler(this.timer0_Tick);
+            // 
+            // Timer1a
+            // 
+            this.Timer1a.Tick += new System.EventHandler(this.timer1a_Tick);
+            // 
+            // Timer1b
+            // 
+            this.Timer1b.Tick += new System.EventHandler(this.timer1b_Tick);
+            // 
+            // Timer2a
+            // 
+            this.Timer2a.Tick += new System.EventHandler(this.timer2a_Tick);
+            // 
+            // Timer2b
+            // 
+            this.Timer2b.Tick += new System.EventHandler(this.timer2b_Tick);
+            // 
+            // Timer3a
+            // 
+            this.Timer3a.Tick += new System.EventHandler(this.timer3a_Tick);
+            // 
+            // Timer3b
+            // 
+            this.Timer3b.Tick += new System.EventHandler(this.timer3b_Tick);
+            // 
+            // Timer4a
+            // 
+            this.Timer4a.Tick += new System.EventHandler(this.timer4a_Tick);
+            // 
+            // Timer4b
+            // 
+            this.Timer4b.Tick += new System.EventHandler(this.timer4b_Tick);
             // 
             // dataGridView1
             // 
@@ -135,7 +180,8 @@
             this.Column2,
             this.Column3});
             this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
-            this.dataGridView1.Location = new System.Drawing.Point(6, 61);
+            this.dataGridView1.Location = new System.Drawing.Point(4, 50);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
             this.dataGridView1.MultiSelect = false;
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
@@ -145,9 +191,10 @@
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dataGridView1.ShowCellErrors = false;
             this.dataGridView1.ShowRowErrors = false;
-            this.dataGridView1.Size = new System.Drawing.Size(598, 250);
+            this.dataGridView1.Size = new System.Drawing.Size(448, 203);
             this.dataGridView1.StandardTab = true;
             this.dataGridView1.TabIndex = 9;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // Column1
             // 
@@ -185,38 +232,44 @@
             // dataGridView2
             // 
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Location = new System.Drawing.Point(6, 61);
+            this.dataGridView2.Location = new System.Drawing.Point(4, 50);
+            this.dataGridView2.Margin = new System.Windows.Forms.Padding(2);
             this.dataGridView2.Name = "dataGridView2";
             this.dataGridView2.RowHeadersWidth = 51;
-            this.dataGridView2.Size = new System.Drawing.Size(610, 250);
+            this.dataGridView2.Size = new System.Drawing.Size(458, 203);
             this.dataGridView2.TabIndex = 0;
             // 
             // dataGridView3
             // 
             this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView3.Location = new System.Drawing.Point(6, 55);
+            this.dataGridView3.Location = new System.Drawing.Point(4, 45);
+            this.dataGridView3.Margin = new System.Windows.Forms.Padding(2);
             this.dataGridView3.Name = "dataGridView3";
             this.dataGridView3.RowHeadersWidth = 51;
             this.dataGridView3.RowTemplate.Height = 24;
-            this.dataGridView3.Size = new System.Drawing.Size(598, 250);
+            this.dataGridView3.Size = new System.Drawing.Size(448, 203);
             this.dataGridView3.TabIndex = 10;
             // 
             // dataGridView4
             // 
             this.dataGridView4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView4.Location = new System.Drawing.Point(6, 55);
+            this.dataGridView4.Location = new System.Drawing.Point(4, 45);
+            this.dataGridView4.Margin = new System.Windows.Forms.Padding(2);
             this.dataGridView4.Name = "dataGridView4";
             this.dataGridView4.RowHeadersWidth = 51;
             this.dataGridView4.RowTemplate.Height = 24;
-            this.dataGridView4.Size = new System.Drawing.Size(610, 250);
+            this.dataGridView4.Size = new System.Drawing.Size(458, 203);
             this.dataGridView4.TabIndex = 11;
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.dataGridView1);
-            this.groupBox1.Location = new System.Drawing.Point(12, 30);
+            this.groupBox1.Location = new System.Drawing.Point(10, 25);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(610, 311);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(485, 275);
             this.groupBox1.TabIndex = 12;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "groupBox1";
@@ -224,9 +277,11 @@
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.dataGridView2);
-            this.groupBox2.Location = new System.Drawing.Point(628, 30);
+            this.groupBox2.Location = new System.Drawing.Point(505, 25);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(622, 311);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Size = new System.Drawing.Size(485, 275);
             this.groupBox2.TabIndex = 13;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "groupBox2";
@@ -234,9 +289,11 @@
             // groupBox3
             // 
             this.groupBox3.Controls.Add(this.dataGridView3);
-            this.groupBox3.Location = new System.Drawing.Point(12, 347);
+            this.groupBox3.Location = new System.Drawing.Point(9, 310);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(610, 311);
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox3.Size = new System.Drawing.Size(485, 275);
             this.groupBox3.TabIndex = 14;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "groupBox3";
@@ -244,34 +301,39 @@
             // groupBox4
             // 
             this.groupBox4.Controls.Add(this.dataGridView4);
-            this.groupBox4.Location = new System.Drawing.Point(628, 347);
+            this.groupBox4.Location = new System.Drawing.Point(505, 310);
+            this.groupBox4.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Size = new System.Drawing.Size(622, 311);
+            this.groupBox4.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox4.Size = new System.Drawing.Size(485, 275);
             this.groupBox4.TabIndex = 15;
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "groupBox4";
-            //
-            // Timer1
-            //
-            this.Timer1.Tick += new System.EventHandler(this.timer1_Tick);
-            //
-            // Timer2
-            //
-            this.Timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "label1";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.ClientSize = new System.Drawing.Size(1000, 600);
             this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.toolStrip1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form_ChangedSize);
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -279,6 +341,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).EndInit();
             this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             this.groupBox3.ResumeLayout(false);
             this.groupBox4.ResumeLayout(false);
@@ -307,8 +370,16 @@
         private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.GroupBox groupBox4;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Timer Timer1;
-        private System.Windows.Forms.Timer Timer2;
+        private System.Windows.Forms.Timer Timer0;
+        private System.Windows.Forms.Timer Timer1a;
+        private System.Windows.Forms.Timer Timer1b;
+        private System.Windows.Forms.Timer Timer2a;
+        private System.Windows.Forms.Timer Timer2b;
+        private System.Windows.Forms.Timer Timer3a;
+        private System.Windows.Forms.Timer Timer3b;
+        private System.Windows.Forms.Timer Timer4a;
+        private System.Windows.Forms.Timer Timer4b;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -16,17 +16,25 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-        string abonent;
-        //static TimerCallback tm = new TimerCallback(Count);
-        //System.Threading.Timer tim = new System.Threading.Timer(tm, 0, 0, 1);
+        private int t0, t1a, t1b, t2a, t2b, t3a, t3b, t4a, t4b;
+        private string abonent;
 
-        // Методы
+        private const int sec = 1000, min = 60000;
+
+        private string[] abonentslist;
+
+        /*private const string[] StandartList = new string[27] {"127.0.0.1", "10.1.1.254", "10.1.2.254", "192.168.60.254", "192.168.68.73", "10.1.2.250", "",
+            "192.168.67.25", "192.168.60.253", "192.168.60.51", "192.168.60.53", "192.168.60.82", "",
+            "10.1.1.3", "10.1.1.4", "10.1.1.5", "10.1.2.1", "10.1.1.1", "192.168.60.81", "10.1.1.80", "10.1.1.7", "",
+            "10.1.1.100", "10.1.3.4", "10.1.3.1", "192.168.249.1", "10.1.11.5"};*/
+
+        #region Методы
         public Form1()
         {
             InitializeComponent();
         }
 
-        public void SomeMethod()
+        private void SomeMethod()
         {
             /*if (textBox1.Text != "")
             {
@@ -38,7 +46,7 @@ namespace WindowsFormsApp1
             }*/
             //Count();
         }
-        
+
         /*public static void Count(Object obj)
         {
             //int x = (int)obj;
@@ -47,36 +55,146 @@ namespace WindowsFormsApp1
             SomeMethod();
         }*/
 
-        // События
-        private void Form1_Load(object sender, EventArgs e)
+        private void PreProcessing()
         {
             if (File.Exists("Abonents.txt"))
             {
                 //int oldw = int.Parse(File.ReadAllText("OldW.txt"));
-                abonent = File.ReadAllText("Abonents.txt");
+                abonentslist = File.ReadAllLines("Abonents.txt");
             }
-            else
-                File.WriteAllText("Abonents.txt", "Some ip"); // .ToString()
+            //else
+                //File.WriteAllLines("Abonents.txt", StandartList); // .ToString()
 
-            dataGridView1.Rows.Add("Laptop 1", "192.168.0.1", "Online");
-            dataGridView1.Rows.Add("Laptop 2", "192.168.51.86", "Online");
-            dataGridView1.Rows.Add("Laptop 3", "127.0.0.1", "Offline");
-            dataGridView1.Rows.Add("Laptop 4", "Some ip", "Offline");
-            dataGridView1.Rows.Add("Laptop 5", "Some ip", "Online");
-            dataGridView1.Rows.Add("Laptop 6", abonent, "Online");
+            /*this.dataGridView1.Rows.Add("Laptop 1", "192.168.0.1", "Online");
+            this.dataGridView1.Rows.Add("Laptop 2", "192.168.51.86", "Online");
+            this.dataGridView1.Rows.Add("Laptop 3", "127.0.0.1", "Offline");
+            this.dataGridView1.Rows.Add("Laptop 4", "Some ip", "Offline");
+            this.dataGridView1.Rows.Add("Laptop 5", "Some ip", "Online");
+            this.dataGridView1.Rows.Add("Laptop 6", abonent, "Online");*/
+
+            t0 = 3 * sec;
+            t1a = 3 * sec;
+            t1b = 1 * min;
+            t2a = 3 * sec;
+            t2b = 1 * min;
+            t3a = 3 * sec;
+            t3b = 1 * min;
+            t4a = 3 * sec;
+            t4b = 1 * min;
         }
 
-        private void timer1_Tick(object sender, EventArgs e)
-        { SomeMethod(); }
+        private void FormResized()
+        {
+            /*if (ClientSize.Height - 60 < label1.Location.X - 44)
+            {
+                pictureBox1.Size = new Size(ClientSize.Height / 2 - 60, ClientSize.Height / 2 - 60);
+                pictureBox2.Size = new Size(ClientSize.Height / 2 - 60, ClientSize.Height / 2 - 60);
+                pictureBox3.Size = new Size(ClientSize.Height / 2 - 60, ClientSize.Height / 2 - 60);
+                pictureBox4.Size = new Size(ClientSize.Height / 2 - 60, ClientSize.Height / 2 - 60);
+            }
+            else
+            {
+                pictureBox1.Size = new Size(label1.Location.X / 2 - 44, label1.Location.X / 2 - 44);
+                pictureBox2.Size = new Size(label1.Location.X / 2 - 44, label1.Location.X / 2 - 44);
+                pictureBox3.Size = new Size(label1.Location.X / 2 - 44, label1.Location.X / 2 - 44);
+                pictureBox4.Size = new Size(label1.Location.X / 2 - 44, label1.Location.X / 2 - 44);
+            }
+
+            pictureBox5.Size = new Size(1, 1);
+            pictureBox2.Location = new Point(pictureBox1.Size.Width + 32, 28);
+            pictureBox3.Location = new Point(12, pictureBox1.Size.Height + 48);
+            pictureBox4.Location = new Point(pictureBox1.Size.Width + 32, pictureBox1.Size.Height + 48);
+            pictureBox5.Location = new Point(pictureBox1.Size.Width * 2 + 52, pictureBox4.Location.Y);*/
+        }
+        #endregion Методы
+
+        #region События
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PreProcessing();
+
+            Timer0.Start();
+            Timer1a.Start();
+            Timer1b.Start();
+            Timer2a.Start();
+            Timer2b.Start();
+            Timer3a.Start();
+            Timer3b.Start();
+            Timer4a.Start();
+            Timer4b.Start();
+
+            Timer0.Interval = 1;
+            Timer1a.Interval = 1;
+            Timer1b.Interval = 1;
+            Timer2a.Interval = 1;
+            Timer2b.Interval = 1;
+            Timer3a.Interval = 1;
+            Timer3b.Interval = 1;
+            Timer4a.Interval = 1;
+            Timer4b.Interval = 1;
+        }
+
+        #region Таймеры
+        private void timer0_Tick(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void timer1a_Tick(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void timer1b_Tick(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void timer2a_Tick(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void timer2b_Tick(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void timer3a_Tick(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void timer3b_Tick(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void timer4a_Tick(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void timer4b_Tick(object sender, EventArgs e)
+        {
+            
+        }
+        #endregion Таймеры
 
         private void button1_Click(object sender, EventArgs e)
         {
             //SomeMethod();
-
-            Timer1.Start();
-            Timer2.Start();
-            Timer1.Interval = 1;
-            Timer2.Interval = 1;
         }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form_ChangedSize(object sender, EventArgs e)
+        {
+            FormResized();
+        }
+        #endregion События
     }
 }
